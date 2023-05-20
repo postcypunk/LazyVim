82ba6660933a84f8eee5571dc37217b0fab2eb4d
Hou Peng <buzzhoot@gmail.com>
2023-05-21 00:12:56 +0800

    feat:fold

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-opt = vim.opt
+local opt = vim.opt
 opt.clipboard = ""
+
+-------------folds---------------
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
