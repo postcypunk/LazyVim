610ac8921428e85c5836390d5cb379f383b9c9a7
Hou Peng <buzzhoot@gmail.com>
2023-05-22 19:14:11 +0800

    try:search index failed
    virtual text not surpport?

--- lua/vscplug/editing.lua
+++ lua/vscplug/editing.lua
@@ -1,4 +1,5 @@
 return {
+  ------------surround
   {
     "echasnovski/mini.surround",
     keys = function(_, keys)
@@ -31,4 +32,36 @@ return {
       },
     },
   },
+
+  ------------HLslens
+
+  -- {
+  --   "kevinhwang91/nvim-hlslens",
+  --   -- config = true,
+  --   config = function()
+  --     require("hlslens").setup()
+  --     local kopts = { noremap = true, silent = true }
+  --
+  --     vim.api.nvim_set_keymap(
+  --       "n",
+  --       "n",
+  --       [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+  --       kopts
+  --     )
+  --     vim.api.nvim_set_keymap(
+  --       "n",
+  --       "N",
+  --       [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+  --       kopts
+  --     )
+  --     vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+  --     vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+  --     vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+  --     vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+  --
+  --     vim.api.nvim_set_keymap("n", "<Leader>l", "<Cmd>noh<CR>", kopts)
+  --   end,
+  -- },
+
+  --
 }
