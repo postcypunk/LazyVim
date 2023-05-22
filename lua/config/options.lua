617dcfcc49c1f4b9456931b1afc2b7fd41e7081c
buzzinv <buzzhoot@hotmail.com>
2023-05-23 05:18:29 +0800

    feat:revertcmd

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-opt.clipboard = ""
+--------------------change LeapBackdrop
+vim.cmd("highlight LeapBackdrop guifg=#777777")
+---------costom cmd  revert file--
+vim.api.nvim_create_user_command("Revert", function()
+  vim.cmd("earlier 1f")
+end, {})
 
+opt.clipboard = ""
 -------------folds---------------
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldenable = false
-vim.cmd("highlight LeapBackdrop guifg=#777777")
