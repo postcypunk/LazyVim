8832d9dd5738e8af3a9693b72179868ca03db144
buzzinv <buzzhoot@hotmail.com>
2023-05-27 16:50:17 +0800

    add transparent background

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -239,4 +239,15 @@ return {
       },
     },
   },
+  {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      vim.g.transparent_groups = vim.list_extend(
+        vim.g.transparent_groups or {},
+        vim.tbl_map(function(v)
+          return v.hl_group
+        end, vim.tbl_values(require("bufferline.config").highlights))
+      )
+    end,
+  },
 }
