44b93bbf942d2ca509046e119fc63713d6a98d8b
PostCyberPunk <lyhguest2@outlook.com>
2023-08-16 01:19:34 +0800

    feat(neogit): try add

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -96,4 +96,13 @@ return {
     "gbprod/substitute.nvim",
     config = true,
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim", -- optional
+    },
+    config = true,
+  },
 }
