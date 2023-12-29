46597955951c2a0bf61490ed0be94dcd277e63b0
PostCyberPunk <lyhguest2@outlook.com>
2023-12-29 18:12:32 +0800

    ft(remove):transparent

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -267,21 +267,21 @@ return {
     --   })
     -- end,
   },
-  {
-    "xiyaowong/transparent.nvim",
-    opts = {
-      extra_groups = {
-        "NotifyBackground",
-      },
-    },
-    config = function()
-      vim.api.nvim_set_hl(0, "NotifyBackground", vim.api.nvim_get_hl_by_name("Normal", true))
-      vim.g.transparent_groups = vim.list_extend(
-        vim.g.transparent_groups or {},
-        vim.tbl_map(function(v)
-          return v.hl_group
-        end, vim.tbl_values(require("bufferline.config").highlights))
-      )
-    end,
-  },
+  -- {
+  --   "xiyaowong/transparent.nvim",
+  --   opts = {
+  --     extra_groups = {
+  --       "NotifyBackground",
+  --     },
+  --   },
+  --   config = function()
+  --     vim.api.nvim_set_hl(0, "NotifyBackground", vim.api.nvim_get_hl_by_name("Normal", true))
+  --     vim.g.transparent_groups = vim.list_extend(
+  --       vim.g.transparent_groups or {},
+  --       vim.tbl_map(function(v)
+  --         return v.hl_group
+  --       end, vim.tbl_values(require("bufferline.config").highlights))
+  --     )
+  --   end,
+  -- },
 }
