cf26d08b589aea53844775c1dc1e985e4b15cab5
PostCyberPunk <lyhguest2@outlook.com>
2024-01-13 18:48:01 +0800

    fix(autocmd)disable auto resize

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,14 +18,18 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
 
 vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
 vim.api.nvim_create_autocmd("User", {
-	pattern = "BDeletePre *",
-	group = "alpha_on_empty",
-	callback = function()
-		local bufnr = vim.api.nvim_get_current_buf()
-		local name = vim.api.nvim_buf_get_name(bufnr)
+  pattern = "BDeletePre *",
+  group = "alpha_on_empty",
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local name = vim.api.nvim_buf_get_name(bufnr)
 
-		if name == "" then
+    if name == "" then
       vim.cmd([[:Alpha | bd#]])
-		end
-	end,
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function() end,
 })
