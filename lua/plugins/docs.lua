84961efde450e99dbe942752bc65772a1dc41479
PostCyberPunk <lyhguest2@outlook.com>
2024-01-14 12:39:55 +0800

    -pretty basic setup

--- lua/plugins/docs.lua
+++ lua/plugins/docs.lua
@@ -1,3 +1,30 @@
 return {
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    -- tag = "*",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {
+            config = {
+              icon_preset= "varied"
+            }
+          }, -- Adds pretty icons to your documents
+          ["core.itero"] = {},
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
 
+        },
+      })
+    end,
+  },
 }
