c9a217bde6efc24d57b9144ee0b3b3a8f53d95ea
PostCyberPunk <lyhguest2@outlook.com>
2024-01-22 18:35:58 +0800

    enable flash search mode,seems like a bug

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -4,7 +4,7 @@ return {
     "folke/flash.nvim",
     opts = function(_, opts)
       opts.modes = {
-        search = { enabled = false },
+        -- search = { enabled = false },
         char = { jump_labels = true, multi_line = false },
       }
       -- opts.char.jump_labels= true
