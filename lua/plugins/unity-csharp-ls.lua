b61e8afb132d9bf99b24fe850fc959f8946f0965
PostCyberPunk <lyhguest2@outlook.com>
2024-01-23 11:08:16 +0800

    sharpier

--- lua/plugins/unity-csharp-ls.lua
+++ lua/plugins/unity-csharp-ls.lua
@@ -4,7 +4,29 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      table.insert(opts.ensure_installed, "csharp-language-server")
+      vim.list_extend(opts.ensure_installed, { "csharp-language-server", "csharpier" })
     end,
+  },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+  --     opts.sources = opts.sources or {}
+  --     table.insert(opts.sources, nls.builtins.formatting.csharpier)
+  --   end,
+  -- },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        cs = { "csharpier" },
+      },
+      formatters = {
+        csharpier = {
+          command = "dotnet-csharpier",
+          args = { "--write-stdout" },
+        },
+      },
+    },
   },
 }
