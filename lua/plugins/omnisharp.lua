cc2c697f3bb91d05007318150875f88ed121f943
PostCyberPunk <buzzhoot@gmail.com>
2024-01-25 21:17:24 +0800

    use custom omnisharp setting

---
+++ lua/plugins/omnisharp.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        omnisharp = {
+          handlers = {
+            ["textDocument/definition"] = function(...)
+              return require("omnisharp_extended").handler(...)
+            end,
+          },
+          keys = {
+            {
+              "gd",
+              function()
+                require("omnisharp_extended").telescope_lsp_definitions()
+              end,
+              desc = "Goto Definition",
+            },
+          },
+          enable_roslyn_analyzers = true,
+          organize_imports_on_format = true,
+          enable_import_completion = true,
+        },
+      },
+    },
+  },
+  -------------Misc-------------
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = opts.ensure_installed or {}
+  --     vim.list_extend(opts.ensure_installed, { "csharpier", "omnisharp" })
+  --   end,
+  -- },
+  { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "c_sharp" })
+      end
+    end,
+  },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = {
+  --     formatters_by_ft = {
+  --       cs = { "csharpier" },
+  --     },
+  --     formatters = {
+  --       csharpier = {
+  --         command = "dotnet-csharpier",
+  --         args = { "--write-stdout" },
+  --       },
+  --     },
+  --   },
+  -- },
+}
