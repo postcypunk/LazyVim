7b07b4dde69e2019c1576492dec49f7fa2b24e07
PostCyberPunk <buzzhoot@gmail.com>
2024-01-26 00:00:17 +0800

    code-lens

--- lua/plugins/omnisharp.lua
+++ lua/plugins/omnisharp.lua
@@ -56,4 +56,32 @@ return {
   --     },
   --   },
   -- },
+  {
+    "VidocqH/lsp-lens.nvim",
+    keys = { { "<leader>uul", "<cmd>LspLensToggle<cr>", desc = "Toggle LspLens" } },
+    opts = {
+      sections = {
+        enable = false,
+        definition = false,
+        references = function(count)
+          return "Ref: " .. count
+        end,
+        -- implements = false,
+        implements = function(count)
+          return "Imp: " .. count
+        end,
+        git_authors = false,
+        -- git_authors = function(latest_author, count)
+        --   return " " .. latest_author .. (count - 1 == 0 and "" or (" + " .. count - 1))
+        -- end,
+      },
+    },
+    config = function(_, opts)
+      local SymbolKind = vim.lsp.protocol.SymbolKind
+      opts.target_symbol_kinds =
+        { SymbolKind.Function, SymbolKind.Method, SymbolKind.Interface, SymbolKind.Field, SymbolKind.Property }
+      opts.wrapper_symbol_kinds = { SymbolKind.Class, SymbolKind.Struct ,SymbolKind.Interface}
+      require("lsp-lens").setup(opts)
+    end,
+  },
 }
