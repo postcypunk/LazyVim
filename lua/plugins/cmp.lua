6f8e83180f5df53611bf2e8d32ea72b2cad87394
PostCyberPunk <lyhguest2@outlook.com>
2024-02-01 20:09:54 +0800

    refactor:move autopairs from cmp to editing

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -118,20 +118,4 @@ return {
       return {}
     end,
   },
-  -- -- then: setup supertab in cmp
-  {
-    "echasnovski/mini.pairs",
-    enabled = false,
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {},
-    config = function(_, opts)
-      require("nvim-autopairs").setup(opts)
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp = require("cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
-  },
 }
--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -124,4 +124,22 @@ return {
       },
     },
   },
+  -- -- then: setup supertab in cmp
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+    config = function(_, opts)
+      require("nvim-autopairs").setup(opts)
+      if require("lazyvim.util").has("cmp") then
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end
+    end,
+  },
 }
