4558369898404492f5b379a3530886815219b5dd
PostCyberPunk <lyhguest2@outlook.com>
2024-02-06 23:37:48 +0800

    diffview close on panel

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -82,8 +82,15 @@ return {
             layout = "diff2_vertical",
           },
         },
+        keymaps = {
+          file_panel = {
+            { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+          },
+          file_history_panel = {
+            { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } },
+          },
+        },
       })
     end,
   },
-
 }
